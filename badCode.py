833c1131ec4c40d415a9bb1f47a7abe149ad26ab
Nicolas Homble <nhomble@terpmail.umd.edu>
2014-04-05 13:06:42 -0400

    replacing pygame with pygraphviz, we will just display the image in a pygame frame

--- badCode.py
+++ badCode.py
@@ -4,6 +4,7 @@ driver, just call Michael's parser from within here
 '''
 
 import sys, pygame
+import pygraphviz as pgv
 
 def main(argv=None):
 	if argv is None:
@@ -19,20 +20,27 @@ def main(argv=None):
 	# and then create the the data structure we want...
 
 	# a simple array should do just fine since order may not matter
-
-	# and then it just boils down to drawing the darn thing
-	data = [Node("here", 10, 100)]
-
-	# figure this out while parsing so that we can scale the images better
+	data = None
 	maxCalls = 10
 	maxSteps = 100
 
-	frame = Frame(maxCalls, maxSteps)
-	frame.create(data)
+	graph = Graph(maxCalls, maxSteps)
+	graph.make(data)
 
-	# troll
-	while True:
-		pass
+	frame = Frame(graph)
+
+class Graph():
+	def __init__(self, maxCalls, maxSteps):
+		self.graph = pgv.AGraph(directed=True))
+		self.maxCalls = maxCalls
+		self.maxSteps = maxSteps
+	
+		self.graph.layout()
+
+	# data better be an array of nodes
+	def make(self, data):
+		if data is None:
+			raise Exception('fuck you data is None')
 
 # node in our graph is just a function call and its stats
 # we also track who a node calls, but we do not care about its parent
@@ -46,38 +54,12 @@ class Node():
 
 # actual pygame frame, for now let's just have it wrapped in a class
 class Frame():
-	def __init__(self, maxCalls, maxSteps):
+	def __init__(self, graph):
 		pygame.init()
+		self.graph
+		self.file = '.tmpfileforbadCODEEDEDE.png'
 
-		self.maxCalls = maxCalls
-		self.maxSteps = maxSteps
-		self.maxRadius = 100
-		self.maxRed = 255
-
-		self.processed = []	
-		self.font = pygame.font.SysFont("monospace", 18)
-
-	# MEESH, use a pmquad tree to organize the nodes in our grid nicely
-	def create(self, data):
-		if data is None:
-			raise Exception('data is none')
-		self.length = len(data)*2*self.maxRadius
-		self.screen = pygame.display.set_mode((self.length, self.length))
-		self.screen.fill((255, 255, 255))
-
-		self.draw_node(data[0], 100, 100)
-
-		pygame.display.update()
-	# drawing idea, (x,y) found from data structure
-	def draw_node(self, node, x, y):
-		self.processed.append(node.name)
-		
-		radius = self.maxRadius * (self.maxCalls // node.calls)
-		red = self.maxRed * (self.maxSteps / node.steps)
-		pygame.draw.circle(self.screen, (red, 0, 0), (x, y), radius, 0)	
-		
-		label = self.font.render(node.name, 1, (255, 255, 255))
-		self.screen.blit(label, (x, y))
+		self.graph.draw(self.file, 'png')
 		
 
 if __name__ == "__main__":
